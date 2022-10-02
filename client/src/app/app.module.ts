df9a175ad660a911f745e2f4a72b2072b733f97d
unknown <philipmodino@gmail.com>
2022-10-02 10:09:29 +0200

    Add Username Component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './containers/chat/chat.component';
+import { UsernameComponent } from './components/username/username.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    UsernameComponent
   ],
   imports: [
     BrowserModule,
